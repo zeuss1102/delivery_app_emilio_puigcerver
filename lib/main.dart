2186d3a66aec9bfd4c198ca4fd6e013f08e06318
zeuss1102 <149445249+zeuss1102@users.noreply.github.com>
2024-11-13 07:53:07 -0600

    iniciando el login_page

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:delivery_app_emilio_puigcerver/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp (
+      debugShowCheckedModeBanner: false,
+      home:LoginPage()
+    );
+  }
+}
+
