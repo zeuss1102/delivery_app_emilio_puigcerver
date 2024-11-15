2a7486555d3d6e2f22c6f077eae42141e6698373
zeuss1102 <149445249+zeuss1102@users.noreply.github.com>
2024-11-14 23:50:15 -0600

    restaurant model

--- lib/componets/my_tab_bar.dart
+++ lib/componets/my_tab_bar.dart
@@ -1,1 +1,34 @@
 import 'package:flutter/material.dart';
+//este es el apartado de pestañas
+
+class MyTabBar extends StatelessWidget {
+  final TabController tabController;
+
+  const MyTabBar({
+    super.key,
+    required this.tabController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      child: TabBar(
+        controller: tabController,
+        tabs: [
+          //1er tab
+          Tab (
+            icon: Icon(Icons.home),
+          ),
+           //2do tab
+          Tab (
+            icon: Icon(Icons.settings),
+          ),
+            //3er tab
+          Tab (
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:delivery_app_emilio_puigcerver/componets/my_current_location.dar
 import 'package:delivery_app_emilio_puigcerver/componets/my_description_box.dart';
 import 'package:delivery_app_emilio_puigcerver/componets/my_drawer.dart';
 import 'package:delivery_app_emilio_puigcerver/componets/my_sliver_app_bar.dart';
+import 'package:delivery_app_emilio_puigcerver/componets/my_tab_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,15 +12,31 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+
+  // Controlador  de pestañas
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this); // este sirve para dividir y salgan los tab bar en este caso son 3 tres iconos home / settings // person 
+
+  }
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
       body: NestedScrollView(
-        headerSliverBuilder: (context, InnerBoxIsScrolled) => [
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
           MySliverAppBar(
-            title: Text('titulo'),
+            title: MyTabBar(tabController: _tabController ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end ,
               children: [
@@ -36,7 +53,24 @@ class _HomePageState extends State<HomePage> {
               ],
             ),)
         ],
-        body: Container(color: Colors.blue),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            ListView.builder(
+              itemCount: 5, //sirve para contabilizar cuantas veces se va a mostrar un elemento
+              itemBuilder: (context, index) => Text("tab1")
+              ),
+               ListView.builder(
+              itemCount: 5, //sirve para contabilizar cuantas veces se va a mostrar un elemento
+              itemBuilder: (context, index) => Text("tab2")
+              ),
+               ListView.builder(
+              itemCount: 5, //sirve para contabilizar cuantas veces se va a mostrar un elemento
+              itemBuilder: (context, index) => Text("tab3")
+              ),
+          
+          ],
+          ),
         ),
     );
   }
